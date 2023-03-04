44be5aa5d753a7eb9d9052102d74eeb23f5e1f2e
BuildTools <unconfigured@null.spigotmc.org>
2023-03-03 21:32:12 -0500

    subida 2

---
+++ js/index.js
@@ -0,0 +1,23 @@
+console.log("hola");
+
+var a = 4;
+var b = 3;
+
+if(a>b){
+    console.log(a);
+}else if(b>a){
+    console.log(b);
+}
+
+var c = 5
+var d = 4
+var e = 3
+
+if(c>d && c>e){
+    console.log(c);
+}else if(d>e && d>c){
+    console.log(d);
+}else if(e>d && e>c){
+    console.log(e);
+}
+//else if(c<e){
